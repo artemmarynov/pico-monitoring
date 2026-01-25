8f3899d0c758f3232991d53990f8c7507d39b1bd
Yehor Voronovskyi <yehor.voronovskyi@student.tuke.sk>
2026-01-25 18:50:42 +0100

    add AWS ip to allow_origins

--- backend/postgress-websocket/main.py
+++ backend/postgress-websocket/main.py
@@ -124,7 +124,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # твой React origin(ы)
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://13.61.186.202:5173"],  # твой React origin(ы)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
